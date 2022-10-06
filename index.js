8ba6c28336af94895f75220dc5bebaa2202560a0
Naman Gogia <naman114@pop-os.localdomain>
2022-10-06 18:51:18 +0530

    feat: add level-2 milestone

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const http = require("http");
+const readline = require("readline");
+
+const PORT = 3000;
+
+const createServer = (registrationPageFilePath) => {
+  http
+    .createServer(function (request, response) {
+      let url = request.url;
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      let stream;
+      switch (url) {
+        case "/project":
+          stream = fs.createReadStream("project.html");
+          break;
+        case "/registration":
+          stream = fs.createReadStream(registrationPageFilePath);
+          break;
+        default:
+          stream = fs.createReadStream("home.html");
+          break;
+      }
+      stream.pipe(response);
+    })
+    .listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}/`);
+    });
+};
+
+const lineDetail = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+lineDetail.question(
+  "Enter full file path to the registration page (registration.html): ",
+  (path) => {
+    lineDetail.close();
+    path = path.length === 0 ? "registration.html" : path;
+    createServer(path);
+  }
+);
